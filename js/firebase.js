5cc525c469e1bd792a227b15f8bbebe4fbf22aad
Jonathan Cayaban <jeancayaban@Jeans-MacBook-Air-2.local>
2021-02-21 15:11:32 -0800

    Got firebase to work

--- js/firebase.js
+++ js/firebase.js
@@ -1,52 +1,61 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDvovXPcD_0MdIUQ7HYHq4tnXloru-enns",
+    authDomain: "localbread-fea00.firebaseapp.com",
+    databaseURL: "https://localbread-fea00-default-rtdb.firebaseio.com",
+    projectId: "localbread-fea00",
+    storageBucket: "localbread-fea00.appspot.com",
+    messagingSenderId: "671775974189",
+    appId: "1:671775974189:web:c2faeacd5bac412da6a9a4",
+    measurementId: "G-JFW650YZEM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
+  const auth = firebase.auth();
 
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
+  function processSignUp() {
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDvovXPcD_0MdIUQ7HYHq4tnXloru-enns",
-  authDomain: "localbread-fea00.firebaseapp.com",
-  databaseURL: "https://localbread-fea00-default-rtdb.firebaseio.com",
-  projectId: "localbread-fea00",
-  storageBucket: "localbread-fea00.appspot.com",
-  messagingSenderId: "671775974189",
-  appId: "1:671775974189:web:10ab59d4db6e0001a6a9a4",
-  measurementId: "G-37GCPQP7CK"
-};
+    var email = document.getElementById("email");
+    var pw = document.getElementById("pw");
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+    const promise = auth.createUserWithEmailAndPassword(email.value, pw.value);
+    promise.catch(e => alert(e.message));
 
-const auth = firebase.auth();
+    alert("Signed Up");
 
-// create instance of Google provider
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    /** @type {firebase.auth.OAuthCredential} */
-    var credential = result.credential;
+  }
 
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+  function processLogIn() {
+
+    var email = document.getElementById("email");
+    var pw = document.getElementById("pw");
+
+    const promise = auth.signInWithEmailAndPassword(email.value, pw.value);
+    promise.catch(e => alert(e.message));
+
+    alert("Logged On");
+
+  }
+
+  function processSignOut() {
+
+    auth.signOut();
+
+    alert("Signed Out");
+
+  }
+
+  auth.onAuthStateChanged(function(user) {
+
+    if (user){
+
+    }
+
+    else{
+
+    }
+
+  })
